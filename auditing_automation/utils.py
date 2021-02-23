98f9e78829727bf5ba630624ae4a95611426ce42
Georgios Christos Chouliaras <chouligi@gmail.com>
2021-02-23 20:32:29 +0100

    fixed flake8

--- auditing_automation/utils.py
+++ auditing_automation/utils.py
@@ -13,3 +13,17 @@ def get_worksheet_values_from_workbook(wookrbook: Workbook, worksheet_name: str)
 
 def get_columns(data: GeneratorType) -> str:
     return next(data)[0:]
+
+
+def create_new_workbook(output_path: str):
+    # Create a new workbook in the given path
+    return
+
+
+def create_workbook_based_on_template():
+    """
+    This function reads a sheet from workbook A, and creates a new workbook B with a subset
+    of the sheet of workbook A.
+    :return:
+    """
+    return
--- xlwings_project/leadsheet_generator.py
+++ xlwings_project/leadsheet_generator.py
@@ -1,11 +1,9 @@
 import xlwings as xw
 import pandas as pd
 import auditing_automation.utils as utils
+
 INPUT_WORKSHEET_NAME = 'Trial Balance'
 INPUT_MAPPING_COL = 'Input - Mapping'
-
-
-
 
 
 def main():
@@ -14,7 +12,8 @@ def main():
     trial_balance_input_sheet = wb.sheets[0]
 
     df = utils.load_xl_workbook(
-        path='/Users/Fosa/PythonProjects/git_tree/auditing_automation/xlwings_project/test_sheet.xlsx')
+        path='/Users/Fosa/PythonProjects/git_tree/auditing_automation/xlwings_project/test_sheet.xlsx'
+    )
 
     sheet = df
     values = df[INPUT_WORKSHEET_NAME].values
@@ -33,12 +32,12 @@ def main():
     new_workbook = xw.Book()
 
     new_workbook.save(f"{path}{required_mapping_type}-leadsheet.xlsx")
-    #new_workbook.save('test_saving_Excel.xslx')
+    # new_workbook.save('test_saving_Excel.xslx')
 
     # add the formated dataframe in first sheet
-    #new_workbook.sheets[0].range('A1').options(index=False, header=True).value = formatted_dataframe
+    # new_workbook.sheets[0].range('A1').options(index=False, header=True).value = formatted_dataframe
     # store the workbook as
-    #new_workbook.save(path=f"{path}{required_mapping_type}-leadsheet.xlsx")
+    # new_workbook.save(path=f"{path}{required_mapping_type}-leadsheet.xlsx")
 
 
 if __name__ == "__main__":
